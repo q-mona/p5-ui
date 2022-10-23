cc470a0b16aa1dcc95f209f11475015775173ac2
q-mona <441146640@qq.com>
2022-10-23 14:16:22 +0800

    modify crowd

--- src/components/canvas/crowd/crowd.vue
+++ src/components/canvas/crowd/crowd.vue
@@ -1,12 +1,24 @@
 <script setup name='P5Crowd'>
 import { onMounted, reactive, ref, watch } from 'vue';
-import crowd_imgs from './image.js'
+import c0 from '../../../assets/images/crowd/crowd0.png'
+import c1 from '../../../assets/images/crowd/crowd1.png'
+import c2 from '../../../assets/images/crowd/crowd2.png'
+import c3 from '../../../assets/images/crowd/crowd3.png'
+import c4 from '../../../assets/images/crowd/crowd4.png'
+import c5 from '../../../assets/images/crowd/crowd5.png'
+import c6 from '../../../assets/images/crowd/crowd6.png'
+import c7 from '../../../assets/images/crowd/crowd7.png'
+import c8 from '../../../assets/images/crowd/crowd8.png'
+import c9 from '../../../assets/images/crowd/crowd9.png'
+import c10 from '../../../assets/images/crowd/crowd10.png'
+import c11 from '../../../assets/images/crowd/crowd11.png'
 
 const props = defineProps({
     config: { type: Object, default: {} },
     idx: { type: String, default: '' }
 })
 
+const crowd_imgs = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11]
 const p5_crowd = ref(null)
 const img_list = []
 const img_num = crowd_imgs.length
